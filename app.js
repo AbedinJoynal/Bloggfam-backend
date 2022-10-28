2b2db3744e1d07c1c9381b7d8fd0ec7826ec2f2a
JoynalAbedin <jolobangladesh@gmail.com>
2022-10-28 17:20:52 +0600

    made some change

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.json("server is connected")
+})
 app.use('/api/user', router);
 app.use('/api/blog', blogRouter);
 const DB = process.env.MONGO_URI;
