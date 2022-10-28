827a7e99325e30d1a7603954d6a989baafad1409
JoynalAbedin <jolobangladesh@gmail.com>
2022-10-28 17:13:46 +0600

    making deploys

--- app.js
+++ app.js
@@ -3,27 +3,16 @@ import mongoose from 'mongoose';
 import blogRouter from './routes/blog-routes';
 import router from './routes/user-routes';
 import cors from 'cors';
-require('dotenv').config();
-const path = require('path');
+import * as dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/user', router);
 app.use('/api/blog', blogRouter);
-//setting mongodb to env
+const DB = process.env.MONGO_URI;
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    //setting port
+    .connect(DB)
     .then(() => app.listen(process.env.PORT || 5000))
     .then(() => console.log('connected to db and server'))
     .catch((err) => console.log(err));
-//setting build
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/frontend/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/frontend/build', 'index.html'));
-    });
-}
